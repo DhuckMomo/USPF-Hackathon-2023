0f53538ad475ff28c143004bc6eb7535eeb11974
Yman <ymanneil07@gmail.com>
2023-12-09 04:22:02 +0800

    form

---
+++ hackathon/app.py
@@ -0,0 +1,98 @@
+from flask import Flask, render_template, request, jsonify
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SelectField, DateField, TimeField, RadioField
+from datetime import datetime
+import os
+import json
+import requests
+from form import IncidentReportForm
+
+
+#severity simple AI
+def analyze_severity(severity):
+    severity_messages = {
+        'very_low': "The incident is considered very low severity. It may not require immediate attention.",
+        'low': "The incident is considered low severity. It may not require immediate attention.",
+        'medium': "The incident is of medium severity. Please address it promptly.",
+        'high': "The incident is of high severity. Immediate attention and action are recommended.",
+        'very_high': "The incident is considered very high severity. Immediate action is required.",
+    }
+    return severity_messages.get(severity, "Severity not specified.")
+
+#Cebu Sentinel
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+
+
+def is_internet_available():
+    try:
+        requests.get("https://google.com", timeout=5)
+        return True
+    except requests.exceptions.ConnectionError:
+        return False
+
+
+
+@app.route('/')
+def index():
+    form = IncidentReportForm()
+    # Mock severity_insights for demonstration purposes
+    severity_insights = "This is a sample severity insight."
+    return render_template('incident_form.html', form=form, severity_insights=severity_insights)
+    #return render_template('incident_form.html', form=form)
+
+
+
+
+@app.route('/submit_report', methods=['POST'])
+def submit_report():
+    form = IncidentReportForm()
+    severity_insights = analyze_severity(form.severity.data)
+    if form.validate_on_submit():
+        try:
+            date_time = datetime.combine(form.date.data, form.time.data)
+        except ValueError as e:
+            return jsonify({'success': False, 'errors': {'date_time': [str(e)]}})
+
+        incident_data = {
+            'incident_type': form.incident_type.data,
+            'date_time': date_time,
+            'location': form.location.data,
+            'barangay': form.barangay.data,
+            'description': form.description.data
+        }
+        
+
+        if is_internet_available():
+            print("Internet connection is available")
+            #  logic for submitting data to server here
+            # ...
+            return jsonify({'success': True, 'message': 'Incident report submitted successfully.', 'severity_insights': severity_insights})
+        else:
+            print("No internet connection available")
+            with open('incident_data.json', 'a') as f:
+                f.write(json.dumps(incident_data) + '\n')
+
+            # Show message about offline storage and severity insights
+            return jsonify({'success': True, 'message': 'Incident report saved offline.', 'severity_insights': severity_insights})
+
+    return jsonify({'success': False, 'errors': form.errors})
+
+
+@app.route('/report.html')
+def report():
+    return render_template('report.html')
+
+@app.route('/homepage.html')
+def homepage():
+    return render_template('homepage.html')
+
+@app.route('/incident_form.html')
+def incident_form():
+    form = IncidentReportForm()
+    return render_template('incident_form.html', form=form)
+
+
+if __name__ == '__main__':
+    #cs23.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
--- hackathon/form.py
+++ hackathon/form.py
@@ -1,38 +1,9 @@
-from flask import Flask, render_template, request, jsonify
+# forms.py
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SelectField, DateField, TimeField, RadioField
-from datetime import datetime
-import os
-import json
-import requests
-
-
-#severity simple AI
-def analyze_severity(severity):
-    severity_messages = {
-        'very_low': "The incident is considered very low severity. It may not require immediate attention.",
-        'low': "The incident is considered low severity. It may not require immediate attention.",
-        'medium': "The incident is of medium severity. Please address it promptly.",
-        'high': "The incident is of high severity. Immediate attention and action are recommended.",
-        'very_high': "The incident is considered very high severity. Immediate action is required.",
-    }
-    return severity_messages.get(severity, "Severity not specified.")
-
-#Cebu Sentinel
-cs23 = Flask(__name__)
-cs23.config['SECRET_KEY'] = 'your_secret_key'
-
-
-def is_internet_available():
-    try:
-        requests.get("https://google.com", timeout=5)
-        return True
-    except requests.exceptions.ConnectionError:
-        return False
-
+from wtforms.validators import DataRequired
 
 class IncidentReportForm(FlaskForm):
-
     BARANGAY_CHOICES = [
         (None, 'Select Barangay'),
         ('adlaon', 'ADLAON'),
@@ -124,69 +95,10 @@ class IncidentReportForm(FlaskForm):
         ('high', 'High'),
         ('very_high', 'Very High'),
     ]
-    severity = RadioField('Severity', choices=SEVERITY_CHOICES)
+    severity = SelectField('Severity', choices=SEVERITY_CHOICES)
     incident_type = SelectField('Incident Type', choices=[('fire', 'Fire'), ('accident', 'Accident'), ('medical', 'Medical Emergency')])
     date = DateField('Date')
     time = TimeField('Time')
     location = StringField('Location')
     barangay = SelectField('Barangay', choices=BARANGAY_CHOICES, default=None)
     description = TextAreaField('Description')
-
-
-@cs23.route('/')
-def index():
-    form = IncidentReportForm()
-    return render_template('incident_form.html', form=form)
-
-
-@cs23.route('/submit_report', methods=['POST'])
-def submit_report():
-    form = IncidentReportForm()
-    severity_insights = analyze_severity(form.severity.data)
-    if form.validate_on_submit():
-        try:
-            date_time = datetime.combine(form.date.data, form.time.data)
-        except ValueError as e:
-            return jsonify({'success': False, 'errors': {'date_time': [str(e)]}})
-
-        incident_data = {
-            'incident_type': form.incident_type.data,
-            'date_time': date_time,
-            'location': form.location.data,
-            'barangay': form.barangay.data,
-            'description': form.description.data
-        }
-        
-
-        if is_internet_available():
-            print("Internet connection is available")
-            #  logic for submitting data to server here
-            # ...
-            return jsonify({'success': True, 'message': 'Incident report submitted successfully.', 'severity_insights': severity_insights})
-        else:
-            print("No internet connection available")
-            with open('incident_data.json', 'a') as f:
-                f.write(json.dumps(incident_data) + '\n')
-
-            # Show message about offline storage and severity insights
-            return jsonify({'success': True, 'message': 'Incident report saved offline.', 'severity_insights': severity_insights})
-
-    return jsonify({'success': False, 'errors': form.errors})
-
-
-@cs23.route('/report.html')
-def report():
-    return render_template('report.html')
-
-@cs23.route('/homepage.html')
-def homepage():
-    return render_template('homepage.html')
-
-@cs23.route('/incident_form.html')
-def incident_form():
-    return render_template('incident_form.html')
-
-
-if __name__ == '__main__':
-    #cs23.run(debug=True)
-    cs23.run(host='0.0.0.0', port=5000, debug=True)
